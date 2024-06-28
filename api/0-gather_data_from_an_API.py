096eaef5fafaeb62a5336d009a148410463c8c1e
Muhodari Sage <sagemuho@gmail.com>
2024-06-28 16:07:52 +0200

    fixed indentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-""" Gather data from an API """
+"""Gather data from an API"""
 import requests
 import sys
 
+
 def get_employee_todo_progress(user_id):
-    """ employees to todo progress """
+    """Fetch and display employee's TODO list progress"""
     try:
         # Fetch employee information
-        user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+        user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+        user_response = requests.get(user_url)
         user_response.raise_for_status()
         user_data = user_response.json()
         employee_name = user_data['name']
 
         # Fetch tasks for the user
-        task_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+        todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+        task_response = requests.get(todos_url)
         task_response.raise_for_status()
         todos_data = task_response.json()
 
-        # count number of completed tasks
+        # Count number of completed tasks
         total_tasks = len(todos_data)
         done_tasks = [todo['title'] for todo in todos_data if todo['completed']]
         number_of_done_tasks = len(done_tasks)
 
         # Print the TODO list progress
-        print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+        print(
+            f"Employee {employee_name} is done with tasks"
+            f"({number_of_done_tasks}/{total_tasks}):"
+        )
         for task in done_tasks:
             print(f"\t {task}")
 
@@ -32,9 +38,10 @@ def get_employee_todo_progress(user_id):
     except KeyError as e:
         print(f"Invalid data received: missing key {e}")
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+        print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     try:
